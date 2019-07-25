524fb84f1908cd55e488bfae85a97f0fac389f22
Adam Converse <aconverse@zendesk.com>
2019-07-24 21:16:23 -0500

    wip

--- app/app.py
+++ app/app.py
@@ -4,9 +4,19 @@ import sys
 import json
 import time
 from flask import Flask, request, abort, jsonify, render_template, current_app
+from redis import Redis
+from rq import Queue
 from __init__ import create_app, redis_url
+import tasks
 
 app = create_app()
+url = redis_url()
+conn = None
+if os.environ.get('REDIS_URL') == "redis":
+  conn = Redis(host='redis', port=6379)
+else:
+  conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+q = Queue(connection=conn)
 
 @app.route('/', methods=['GET'])
 def index():
