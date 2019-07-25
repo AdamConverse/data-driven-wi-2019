a3e81e9b7beb2915fb2fff1d07f9a2ef82f228ff
Adam Converse <aconverse@zendesk.com>
2019-07-24 21:33:29 -0500

    wip

--- app/tasks.py
+++ app/tasks.py
@@ -1,9 +1,10 @@
 import os
 import sys
+import time
 from flask import jsonify
-from datetime import datetime
+from datetime import datetimep
 
 def my_task():
   time = datetime.now()
-  sleep(3)
+  time.sleep(3)
   return jsonify({ "start_time": time, "end_time": datetime.now() })
